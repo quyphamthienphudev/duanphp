98f20a34fcf1a157dd0cde45e230c55085ea48b3
Quy Pham Thien Phu <phamthienphuquy1007@gmail.com>
2025-12-05 18:25:59 +0700

    Update database connection details in connection.php

--- connection.php
+++ connection.php
@@ -1,3 +1,3 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=db2','root','');
+$pdo = new PDO('mysql:host=sql207.byethost7.com;dbname=b7_38296736_duanphp','b7_38296736','Quyquy123@!');
 $pdo->query('set names utf8');
